fb3e25cf7741a814014848094d1f6dfbd50c5937
Asad Mahmood <2325036M@student.gla.ac.uk>
2019-01-28 19:48:18 +0000

    Category Forms Added

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
 from rango.models import Page
+from rango.forms import CategoryForm
 
 def about(request):
     return render(request, 'rango\about.html')
@@ -9,7 +10,8 @@ def about(request):
 def index(request):
 
     category_list = Category.objects.order_by('-likes')[:5]
-    context_dict = {'categories': category_list}
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages': page_list}
     return render(request, 'rango\index.html', context_dict)
 
 def show_category(request, category_name_slug):
@@ -26,3 +28,26 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
 
     return render(request, 'rango/category.html', context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+    else:
+        print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+
+
+
+
+
+
+
+
+        
